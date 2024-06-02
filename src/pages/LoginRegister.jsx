e5a333f94d985f628e5fd359b7dedef5f60c2f17
ihwan4rfa <ihwanarifandi@gmail.com>
2024-06-02 10:32:21 +0700

    update responsive layout for login and register

--- src/pages/LoginRegister.jsx
+++ src/pages/LoginRegister.jsx
@@ -137,9 +137,9 @@ const MainContent = () => {
     const data = useContext(ThemeContext);
 
     return (
-        <div className="flex flex-col items-center justify-center w-full h-screen font-montserrat bg-gradient-to-r from-slate-200 to-slate-100 dark:from-slate-800 dark:to-slate-700">
-            <div className={`bg-white dark:bg-slate-900 rounded-[30px] translate-y-[4%] shadow-lg dark:shadow-slate-700 relative overflow-hidden w-1/2 max-w-full min-h-[400px] transition-all ease-in-out ${data.themeButtonClicked ? 'duration-0' : 'duration-300'} ${transitionOut ? 'opacity-0' : ''} ${transitionIn ? 'opacity-0' : ''}`}>
-                <div className={`absolute top-0 h-full transition-all duration-500 ease-in-out w-1/2 ${registerClicked ? 'z-20 translate-x-[100%]' : 'z-10 translate-x-[0%]'}`}>
+        <div className="flex items-center justify-center w-full h-screen font-montserrat bg-gradient-to-r from-slate-200 to-slate-100 dark:from-slate-800 dark:to-slate-700">
+            <div className={`flex flex-col text-center bg-white dark:bg-slate-900 rounded-[30px] translate-y-[4%] shadow-lg dark:shadow-slate-700 relative overflow-hidden lg:w-1/2 md:w-2/3 w-3/4 max-w-full md:min-h-[400px] min-h-[550px] transition-all ease-in-out ${data.themeButtonClicked ? 'duration-0' : 'duration-300'} ${transitionOut ? 'opacity-0' : ''} ${transitionIn ? 'opacity-0' : ''}`}>
+                <div className={`absolute top-0 md:h-full h-2/3 transition-all duration-500 ease-in-out md:w-1/2 w-full ${registerClicked ? 'z-20 md:translate-x-[100%] -translate-x-[0%]' : 'z-10 md:translate-x-[0%] translate-x-[100%]'}`}>
                     <div className="flex flex-col items-center justify-center h-full px-10 bg-white dark:bg-slate-900">
                         <h1 className="text-2xl font-semibold tracking-tight text-slate-900 dark:text-white">Create Account</h1>
                         <div className="mt-2 mb-3">
@@ -153,12 +153,12 @@ const MainContent = () => {
                             <input value={passwordRegister} onChange={handleChangePasswordRegister} className="w-full outline-none bg-slate-200 placeholder:text-slate-400 text-slate-900 dark:text-white dark:bg-slate-800 dark:placeholder:text-slate-500" type={seePassword ? 'text' : 'password'} placeholder="Password" />
                             <button onClick={toggleSeePassword}><i className={`text-slate-400 dark:text-slate-500 fa-solid ${seePassword ? 'fa-eye' : 'fa-eye-slash'}`}></i></button>
                         </div>
-                        {tokenRegister ? <h1 className="text-[12px] text-green-500"><i className="mr-1 fa-solid fa-circle-check"></i>registration success!</h1> : ""}
+                        {tokenRegister ? <h1 className="text-[12px] text-teal-500"><i className="mr-1 fa-solid fa-circle-check"></i>registration success!</h1> : ""}
                         {errorRegister ? <h1 className="text-[12px] text-red-500 text-center"><i className="mr-1 fa-solid fa-triangle-exclamation"></i>{errorRegister}</h1> : ""}
                         <button onClick={handleRegister} className="bg-blue-600 hover:bg-blue-700 text-white text-[12px] py-[10px] px-8 rounded-lg font-semibold tracking-tight uppercase mt-3">Register</button>
                     </div>
                 </div>
-                <div className={`absolute top-0 h-full transition-all duration-500 ease-in-out w-1/2 ${loginClicked ? 'z-20 translate-x-[0%]' : 'z-10 translate-x-[100%]'}`}>
+                <div className={`absolute top-0 md:h-full h-2/3 transition-all duration-500 ease-in-out md:w-1/2 w-full ${loginClicked ? 'z-20 translate-x-[0%]' : 'z-10 md:translate-x-[100%] -translate-x-[100%]'}`}>
                     <div className="flex flex-col items-center justify-center h-full px-10 bg-white dark:bg-slate-900">
                         <h1 className="text-2xl font-semibold tracking-tight text-slate-900 dark:text-white">Log In</h1>
                         <div className="mt-2 mb-3">
@@ -172,22 +172,22 @@ const MainContent = () => {
                             <input value={password} onChange={handleChangePassword} className="w-full outline-none bg-slate-200 placeholder:text-slate-400 text-slate-900 dark:text-white dark:bg-slate-800 dark:placeholder:text-slate-500" type={seePassword ? 'text' : 'password'} placeholder="Password" />
                             <button onClick={toggleSeePassword}><i className={`text-slate-400 dark:text-slate-500 fa-solid ${seePassword ? 'fa-eye' : 'fa-eye-slash'}`}></i></button>
                         </div>
-                        {token ? <h1 className="text-[12px] text-green-500"><i className="mr-1 fa-solid fa-circle-check"></i>log In success!</h1> : ""}
+                        {token ? <h1 className="text-[12px] text-teal-500"><i className="mr-1 fa-solid fa-circle-check"></i>log In success!</h1> : ""}
                         {errorLogin ? <h1 className="text-[12px] text-red-500 text-center"><i className="mr-1 fa-solid fa-triangle-exclamation"></i>Log In failed! {errorLogin}</h1> : ""}
                         <button onClick={handleLogin} className="bg-teal-600 hover:bg-teal-700 text-white text-[12px] py-[10px] px-8 rounded-lg font-semibold tracking-tight uppercase mt-3">Log In</button>
                     </div>
                 </div>
-                <div className={`absolute top-0 left-1/2 w-1/2 h-full overflow-hidden transition-all duration-500 ease-in-out z-30 ${btnClicked ? '-translate-x-[100%] rounded-r-[100px]' : 'translate-x-[0%] rounded-l-[100px]'}`}>
+                <div className={`absolute md:top-0 top-1/2 md:left-1/2 left-0 md:w-1/2 w-full md:h-full h-1/3 overflow-hidden transition-all duration-500 ease-in-out z-30 md:translate-y-[0%] translate-y-[50%] rounded-t-[50px] ${btnClicked ? 'md:-translate-x-[100%] md:rounded-r-[100px] md:rounded-tl-[0px] rounded-r-[0px]' : 'md:translate-x-[0%] md:rounded-l-[100px] md:rounded-tr-[0px] rounded-l-[0px]'}`}>
                     <div className={`bg-gradient-to-r from-blue-600 to-teal-600 h-full text-white relative -left-[100%] w-[200%] transition-all duration-500 ${btnClicked ? 'translate-x-[50%]' : 'translate-x-[0%]'}`}>
-                        <div className="absolute w-1/2 h-full flex flex-col items-center justify-center px-[30px] text-center top-0 transition-all duration-500 ease-in-out translate-x-[0%]">
-                            <h1 className="text-2xl font-semibold tracking-tight">Welcome Back!</h1>
-                            <p className="text-sm my-5 mx-[25px]">Enter your secret identity to discover all our members!</p>
-                            <button onClick={toggleButton} className="text-white text-[12px] py-[10px] px-8 rounded-lg font-semibold tracking-tight uppercase mt-3 border hover:bg-white hover:text-blue-700">Log In</button>
+                        <div className={`absolute w-1/2 h-full flex flex-col items-center justify-center px-[30px] text-center top-0 transition-all duration-500 ease-in-out translate-x-[0%]`}>
+                            <h1 className="text-xl font-semibold tracking-tight md:text-2xl">Welcome Back!</h1>
+                            <p className="md:text-sm text-xs md:my-5 my-1 mx-[25px]">Enter your secret identity to discover all our members!</p>
+                            <button onClick={toggleButton} className="text-white md:text-[12px] text-[10px] md:py-[10px] py-[8px] px-8 rounded-lg font-semibold tracking-tight uppercase mt-3 border hover:bg-white hover:text-blue-700">Log In</button>
                         </div>
-                        <div className="absolute w-1/2 h-full flex flex-col items-center justify-center px-[30px] text-center top-0 transition-all duration-500 ease-in-out translate-x-[100%]">
-                            <h1 className="text-2xl font-semibold tracking-tight">New here?</h1>
-                            <p className="text-sm my-5 mx-[25px]">Create an account with your secret identity and join our community!</p>
-                            <button onClick={toggleButton} className="text-white text-[12px] py-[10px] px-8 rounded-lg font-semibold tracking-tight uppercase mt-3 border hover:bg-white hover:text-teal-700">Register</button>
+                        <div className={`absolute w-1/2 h-full flex flex-col items-center justify-center px-[30px] text-center top-0 transition-all duration-500 ease-in-out translate-x-[100%]`}>
+                            <h1 className="text-xl font-semibold tracking-tight md:text-2xl">New here?</h1>
+                            <p className="md:text-sm text-xs md:my-5 my-1 mx-[25px]">Create an account with your secret identity and join our community!</p>
+                            <button onClick={toggleButton} className="text-white md:text-[12px] text-[10px] md:py-[10px] py-[8px] px-8 rounded-lg font-semibold tracking-tight uppercase mt-3 border hover:bg-white hover:text-teal-700">Register</button>
                         </div>
                     </div>
                 </div>
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -24,15 +24,19 @@ const Navbar = () => {
     const handleThemeSwitch = () => {
         data.setThemeButtonClicked(true);
         data.setTheme(data.theme === "dark" ? "light" : "dark");
+        setTimeout(() => {
+            data.setThemeButtonClicked(false);
+        }, 500)
     }
 
     return (
         <div className={`fixed z-50 flex w-full h-10 transition-all ${data.themeButtonClicked ? 'duration-0' : 'duration-300'} ease-in-out bg-white dark:bg-slate-900 shadow-md font-montserrat dark:shadow-slate-700 ${transitionIn && !token ? 'opacity-0' : ''}`}>
             <div className="flex items-center justify-between w-full mx-16">
                 <Link to={"/"}><h1 className="font-semibold text-slate-900 dark:text-white"><i className="mr-3 text-lg text-blue-600 fa-solid fa-user-secret dark:text-teal-600"></i>SpyMasters</h1></Link>
-                <div className="flex gap-5 text-sm font-semibold">
+                <div className="flex gap-8 text-sm font-semibold">
                     <button onClick={handleThemeSwitch}><i class={`fa-solid ${data.theme === "dark" ? 'fa-sun text-white hover:text-teal-600' : 'fa-moon text-slate-900 hover:text-blue-600 mr-[2px]'}`}></i></button>
                     <Link to="/"><p className="cursor-pointer text-slate-900 dark:text-white hover:text-blue-600 dark:hover:text-teal-400">Home</p></Link>
+                    <a target="_blank" href="https://github.com/ihwan4rfa" className="cursor-pointer text-slate-900 dark:text-white hover:text-blue-600 dark:hover:text-teal-400">Contact Us</a>
                     <Link to="/login" onClick={handleLogout}><p className="cursor-pointer text-slate-900 dark:text-white hover:text-blue-600 dark:hover:text-teal-400">{token ? 'Logout' : 'Log In'}</p></Link>
                 </div>
             </div>
