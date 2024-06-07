29629bda27d0830b2a6a334cea3d56a55d27821f
ihwan4rfa <ihwanarifandi@gmail.com>
2024-06-07 14:10:43 +0700

    add mobile navbar/responsive navbar

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useContext } from "react";
 import { ThemeContext } from "../context/themeContext";
+import NavbarMobile from "./NavbarMobile";
 
 const Navbar = () => {
 
@@ -29,18 +30,29 @@ const Navbar = () => {
         }, 500)
     }
 
+    const [isToggleMenuClicked, setIsToggleMenuCliccked] = useState(false);
+    const handleToggleMenu = () => {
+        setIsToggleMenuCliccked(!isToggleMenuClicked);
+    }
+
     return (
-        <div className={`fixed z-50 flex w-full h-10 transition-all ${data.themeButtonClicked ? 'duration-0' : 'duration-300'} ease-in-out bg-white dark:bg-slate-900 shadow-md font-montserrat dark:shadow-slate-700 ${transitionIn && !token ? 'opacity-0' : ''}`}>
-            <div className="flex items-center justify-between w-full mx-16">
-                <Link to={"/"}><h1 className="font-semibold text-slate-900 dark:text-white"><i className="mr-3 text-lg text-blue-600 fa-solid fa-user-secret dark:text-teal-600"></i>SpyMasters</h1></Link>
-                <div className="flex gap-8 text-sm font-semibold">
-                    <button onClick={handleThemeSwitch}><i class={`fa-solid ${data.theme === "dark" ? 'fa-sun text-white hover:text-teal-600' : 'fa-moon text-slate-900 hover:text-blue-600 mr-[2px]'}`}></i></button>
-                    <Link to="/"><p className="cursor-pointer text-slate-900 dark:text-white hover:text-blue-600 dark:hover:text-teal-400">Home</p></Link>
-                    <a target="_blank" href="https://github.com/ihwan4rfa" className="cursor-pointer text-slate-900 dark:text-white hover:text-blue-600 dark:hover:text-teal-400">Contact Us</a>
-                    <Link to="/login" onClick={handleLogout}><p className="cursor-pointer text-slate-900 dark:text-white hover:text-blue-600 dark:hover:text-teal-400">{token ? 'Logout' : 'Log In'}</p></Link>
+        <>
+            <div className={`fixed z-50 flex w-full h-10 transition-all ${data.themeButtonClicked ? 'duration-0' : 'duration-300'} ease-in-out bg-white dark:bg-slate-900 shadow-md font-montserrat dark:shadow-slate-700 ${transitionIn && !token ? 'opacity-0' : ''}`}>
+                <div className="flex items-center justify-between w-full mx-6 md:mx-16">
+                    <Link to={"/"}><h1 className="font-semibold text-slate-900 dark:text-white"><i className="mr-3 text-lg text-blue-600 fa-solid fa-user-secret dark:text-teal-600"></i>SpyMasters</h1></Link>
+                    <div className="flex gap-6 text-sm font-semibold">
+                        <button onClick={handleThemeSwitch}><i class={`fa-solid ${data.theme === "dark" ? 'fa-sun text-white hover:text-teal-600' : 'fa-moon text-slate-900 hover:text-blue-600 mr-[2px]'}`}></i></button>
+                        <button className="inline md:hidden" onClick={handleToggleMenu}><i class={`fa-solid text-slate-900 dark:text-white ${isToggleMenuClicked ? 'fa-xmark text-[21px]' : 'fa-bars text-lg'}`}></i></button>
+                        <div className="hidden gap-6 md:flex">
+                            <Link to="/"><p className="cursor-pointer text-slate-900 dark:text-white hover:text-blue-600 dark:hover:text-teal-400">Home</p></Link>
+                            <a target="_blank" href="https://github.com/ihwan4rfa" className="cursor-pointer text-slate-900 dark:text-white hover:text-blue-600 dark:hover:text-teal-400">Contact Us</a>
+                            <Link to="/login" onClick={handleLogout}><p className="cursor-pointer text-slate-900 dark:text-white hover:text-blue-600 dark:hover:text-teal-400">{token ? 'Logout' : 'Log In'}</p></Link>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+            <NavbarMobile token={token} handleLogout={handleLogout} isToggleMenuClicked={isToggleMenuClicked} />
+        </>
     )
 }
 
---
+++ src/pages/NavbarMobile.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { ThemeContext } from "../context/themeContext";
+
+const NavbarMobile = (props) => {
+
+    const { token, handleLogout, isToggleMenuClicked } = props;
+    const data = useContext(ThemeContext);
+
+    return (
+        <div className={`fixed bg-white dark:bg-slate-900 w-full py-5 rounded-b-xl pt-14 z-40 inline md:hidden transition-all ease-in-out ${data.themeButtonClicked ? 'duration-0' : 'duration-300'} shadow-md font-montserrat dark:shadow-slate-700 ${isToggleMenuClicked ? 'translate-y-[0px]' : '-translate-y-[150px]'}`}>
+            <Link to="/"><p className="mx-6 my-2 cursor-pointer text-slate-900 dark:text-white hover:text-blue-600 dark:hover:text-teal-400">Home</p></Link>
+            <a target="_blank" href="https://github.com/ihwan4rfa" className="mx-6 my-2 cursor-pointer text-slate-900 dark:text-white hover:text-blue-600 dark:hover:text-teal-400">Contact Us</a>
+            <Link to="/login" onClick={handleLogout}><p className="mx-6 my-2 cursor-pointer text-slate-900 dark:text-white hover:text-blue-600 dark:hover:text-teal-400">{token ? 'Logout' : 'Log In'}</p></Link>
+        </div>
+    )
+}
+
+export default NavbarMobile;
